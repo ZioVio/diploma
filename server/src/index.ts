d4fa13f9d570f0524e69d2431db0da71e8989c60
ZioVio <anderkoval3@gmail.com>
2021-12-28 12:24:43 +0200

    feat: added morgan and cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,16 @@
 import './config/configure-env';
 
 import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
 
 import { PORT } from './config';
 import { connect } from './db/connect';
 
 const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
 
 app.listen(PORT, () => {
   console.log(`App started on port ${PORT}`);
